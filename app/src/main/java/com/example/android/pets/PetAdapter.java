3ce4aa733c24f83337583734dc71ddb5ca4848b9
Mahima <mahima.6.ms@gmail.com>
2019-07-28 21:11:58 +0530

    create RecyclerView adapter for pets list

---
+++ app/src/main/java/com/example/android/pets/PetAdapter.java
@@ -0,0 +1,51 @@
+package com.example.android.pets;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.android.pets.data.Pet;
+
+import java.util.List;
+
+public class PetAdapter extends RecyclerView.Adapter<PetAdapter.PetViewHolder> {
+
+    private List<Pet> pets;
+
+    @NonNull @Override public PetViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return new PetViewHolder(LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item,
+                viewGroup, false));
+    }
+
+    @Override public void onBindViewHolder(@NonNull PetViewHolder petViewHolder, int i) {
+        Pet pet = pets.get(i);
+        if (pet != null) {
+            petViewHolder.name.setText(pet.getName());
+            petViewHolder.summary.setText(pet.getBreed());
+        }
+    }
+
+    @Override public int getItemCount() {
+        return pets != null ? pets.size() : 0;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+        notifyDataSetChanged();
+    }
+
+    static class PetViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView name;
+        private TextView summary;
+
+        PetViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.name);
+            summary = itemView.findViewById(R.id.summary);
+        }
+    }
+}
